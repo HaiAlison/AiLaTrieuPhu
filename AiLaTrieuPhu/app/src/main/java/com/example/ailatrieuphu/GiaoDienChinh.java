da8d76bde3c0b66c84e00476e527e8a2eae60313
Hồ Tấn Đạt <hotandat260399@gmail.com>
2019-11-14 17:18:37 +0700

    Facebook
    hoàn thiện chức năng đăng nhập-đăng xuất facebook, lấy thông tin người chơi từ facebook

--- AiLaTrieuPhu/app/src/main/java/com/example/ailatrieuphu/GiaoDienChinh.java
+++ AiLaTrieuPhu/app/src/main/java/com/example/ailatrieuphu/GiaoDienChinh.java
@@ -1,22 +1,105 @@
 package com.example.ailatrieuphu;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import com.facebook.share.widget.ShareDialog;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 
 public class GiaoDienChinh extends AppCompatActivity {
+//    private ShareDialog shareDialog;
+    //private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_giao_dien_chinh);
-
+        //gán dữ liệu đã nhận được từ FB từ Main2Activity
+        Bundle inBundle=getIntent().getExtras();
+        String name=inBundle.get("name").toString();
+        String surename=inBundle.get("surename").toString();
+        String imageUrl=inBundle.get("imageUrl").toString();
+        TextView nameView=(TextView)findViewById(R.id.text_UserName);
+        nameView.setText(surename+" "+name+"");
+        Button logout=(Button)findViewById(R.id.btn_Exit);
+        //thiết lập sự kiện thoát khỏi Account FB và thoát khỏi trang hiện tại về trang đăng nhập
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logOut();
+                Intent back=new Intent(GiaoDienChinh.this,Main2Activity.class);
+                startActivity(back);
+                finish();
+            }
+        });
+        //Tải lại giao diện để đổi thành ảnh đại diện trên FB
+        new GiaoDienChinh.DownloadImage((ImageView)findViewById(R.id.iv_avatar)).execute(imageUrl);
     }
     public void ChuyenTrang(View view){
         Intent intent=new Intent(this,MainActivity.class);
         startActivity(intent);
+    }
+    //Tải dữ liệu ảnh đại diện từ FB về và gán vào lại id imageView
+    public class DownloadImage extends AsyncTask<String,Void, Bitmap>{
+        ImageView bmImage;
+        public DownloadImage(ImageView bmImage) {
+            this.bmImage=bmImage;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay=urls[0];
+            Bitmap mIconll=null;
+            try {
+                InputStream in=new java.net.URL(urldisplay).openStream();
+                mIconll= BitmapFactory.decodeStream(in);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return mIconll;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            bmImage.setImageBitmap(result);
+        }
+    }
+    public void ExitAccount(View view) {
 
     }
 }
--- AiLaTrieuPhu/app/src/main/java/com/example/ailatrieuphu/Main2Activity.java
+++ AiLaTrieuPhu/app/src/main/java/com/example/ailatrieuphu/Main2Activity.java
@@ -8,21 +8,136 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Main2Activity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private ProfileTracker profileTracker;
+    private AccessTokenTracker accessTokenTracker;
     //note lỗi
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main2);
+        //Đăng ký gọi lại để xử lý các phản hồi đăng nhập
+        callbackManager = CallbackManager.Factory.create();
+        accessTokenTracker=new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+            }
+        };
+        profileTracker=new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                nextActivity(currentProfile);
+            }
+        };
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        FacebookCallback<LoginResult> callback=new FacebookCallback<LoginResult>() {
+            @Override
+            //thực hiện các hàm, câu lệnh khi đăng nhập thành công
+            public void onSuccess(LoginResult loginResult) {
+                Profile profile=Profile.getCurrentProfile();
+                nextActivity(profile);
+                Toast.makeText(getApplicationContext(),"Loggin in...",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        };
+        //Đăng ký gọi lại để nhận thông tin public trên FB
+        loginButton.setReadPermissions("public_profile");
+        loginButton.registerCallback(callbackManager,callback);
+//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+//                    @Override
+//                    public void onCompleted(JSONObject object, GraphResponse response) {
+//                        Log.v("Main2Activity", response.toString());
+//                    }
+//                });
+//                Bundle parameters = new Bundle();
+//                parameters.putString("fields", "id, name, gender, birthday");
+//                graphRequest.setParameters(parameters);
+//                graphRequest.executeAsync();
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//
+//            }
+//        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Profile profile=Profile.getCurrentProfile();
+        nextActivity(profile);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+    //Gọi callbackManager.onActivityResult
+    //để chuyển kết quả đăng nhập đến LoginManager qua callbackManager
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void launchResignActivity(View view) {
@@ -49,6 +164,20 @@ public class Main2Activity extends AppCompatActivity {
     public void dangNhapGiaoDien(View view) {
         Intent intent=new Intent(this,GiaoDienChinh.class);
         startActivity(intent);
+    }
+    private void nextActivity(Profile profile)
+    {
+        //nếu đăng nhập thành công thì sẽ truyền dữ liệu qua bên Activity khác bao gồm tên và avata
+        if(profile!=null)
+        {
+            Intent intent=new Intent(this,GiaoDienChinh.class);
+            //yêu cầu FB cấp thông tin tên, và ảnh đại diện
+            intent.putExtra("name",profile.getFirstName());
+            intent.putExtra("surename",profile.getLastName());
+            intent.putExtra("imageUrl",profile.getProfilePictureUri(162,181).toString());
+            startActivity(intent);
+        }
+
     }
 }
 
